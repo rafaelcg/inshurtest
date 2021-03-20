6c8d04813b97f7592713c7fe351bd43973e5c28b
Rafael Guglielmi <rafaelcg@gmail.com>
2021-03-20 19:34:21 +0000

    added endpoint testing

--- src/components/Profile/Profile.test.tsx
+++ src/components/Profile/Profile.test.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import Profile from './Profile';
+import axios from 'axios';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<Profile />);
-  const linkElement = getByText(/Profile/i);
-  expect(linkElement).toBeInTheDocument();
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe('Profile tests', () => {
+  it('renders properly', () => {
+    const { getByText } = render(<Profile />);
+    const linkElement = getByText(/Profile/i);
+    expect(linkElement).toBeInTheDocument();
+  })
+
+  it("renders without crashing", async () => {
+
+    const data = {
+      data: {
+        id: '1',
+        name: 'name',
+        jobTitle: 'title',
+        favouriteFood: 'food',
+        photoURL: 'picture'
+      }
+    }
+
+    mockedAxios.get.mockResolvedValue(data)
+
+    const { queryByTestId } = render(<Profile />);
+    const element = queryByTestId('element')
+
+    expect(mockedAxios.get).toHaveBeenCalledWith('http://localhost:4000/users/1');
+
+    await waitFor(() => {
+      expect(element?.textContent).toContain('John Doe');
+    })
+
+  });
 });
